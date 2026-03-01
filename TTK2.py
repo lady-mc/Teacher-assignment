7964633f9ab828a9b4b88476a905716ec78c55a8
lady-mc <marieclaire9255@gmailcom>
2026-03-01 09:08:39 +0100

    online class

---
+++ TTK2.py
@@ -0,0 +1,17 @@
+print("Online Class")
+
+link = input("Enter meeting link: ")
+teacher = input("Teacher name: ")
+student = input("Enter a student name: ")
+
+print(teacher, "joined the meeting.")
+print(student, "joined the meeting.")
+
+print("Class started")
+
+i = 1
+while i <= 60:
+    print("Minute", i)
+    i = i + 1
+
+print("Meeting ended")
